c8f140fda901229a5fbcac870a29aa2e0df7b995
Mdotta <md.correa@outlook.com>
2026-06-21 15:48:51 -0300

    fix: include health endpoints

--- src/Postech.Catalog.Api/Extensions/WebApplicationExtensions.cs
+++ src/Postech.Catalog.Api/Extensions/WebApplicationExtensions.cs
@@ -33,6 +33,7 @@ public static class WebApplicationExtensions
         app.MapScalarApiReference();
 
         // Map Endpoints
+        app.MapHealthEndpoints();
         app.MapCatalogEndpoints();
 
         return app;
